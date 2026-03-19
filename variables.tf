be32764a4a2346eeac4d7edda2a7137974c49549
Vivian Moutinho <113911497+vivimoutinho@users.noreply.github.com>
2026-03-18 22:19:21 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS Region for deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
